78d43d3b10669bf1e419623cf6f472f9df1d4514
tatmush <tatalmondmush@gmail.com>
2018-03-01 13:32:20 +0200

    initial working prototype

--- test1.py
+++ test1.py
@@ -5,7 +5,12 @@ def getManPages(command):
     return subprocess.check_output("man {}".format(command), shell=True)
 
 foobar=open("foobar.txt", "a")
-commands=["arp", "ls", "rm"]
+
+'''text had to be made to a list of commands by
+a="ls, rm, mkdir, rmdir, pwd, ln, chmod, umask, ping, cut, sort, which, grep, whereis, w, who, whoami, last, file, strings, top, ps, nice, nohup, kill, signal, more, less, ifconfig, arp, nslookup, cat, uname, history, netstat, ifconfig, dig, man, lsof, crontab, nc, uniq, id, groups, df, du, touch, ip"
+b=a.split(", ")
+'''
+commands=['ls', 'rm', 'mkdir', 'rmdir', 'pwd', 'ln', 'chmod', 'umask', 'ping', 'cut', 'sort', 'which', 'grep', 'whereis', 'w', 'who', 'whoami', 'last', 'file', 'strings', 'top', 'ps', 'nice', 'nohup', 'kill', 'signal', 'more', 'less', 'ifconfig', 'arp', 'nslookup', 'cat', 'uname', 'history', 'netstat', 'ifconfig', 'dig', 'man', 'lsof', 'crontab', 'nc', 'uniq', 'id', 'groups', 'df', 'du', 'touch']
 for command in commands:
     #get manual pages from standard output. @Return type: bytes
     manPage=getManPages(command)
@@ -17,13 +22,18 @@ for command in commands:
     linesList=manStr.split("\n")
 
     #starting index
+    print(command)
     index=linesList.index("DESCRIPTION")
+
+    #Build perfection by finding complete sentences and ending the description of the tool there.
+    
     foobar.write(command + "\t--")
     for line in linesList[index +1:index+7]:
         foobar.write(line)
     foobar.write("\r\r")
 
 foobar.close()
+print("DONE")
 
 
 
